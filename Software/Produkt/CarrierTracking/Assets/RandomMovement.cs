5f943801795a95fe0ae16783078412df46fc2b8c
ViDo00 <72261969+ViDo00@users.noreply.github.com>
2020-11-04 11:06:24 +0100

    Init?

---
+++ Software/Produkt/CarrierTracking/Assets/RandomMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class RandomMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 min;
+    [SerializeField]
+    private Vector2 max;
+    [SerializeField]
+    private Vector2 yRotationRange;
+    [SerializeField]
+    [Range(0.01f,0.1f)]
+    private float lerpSpeed = 0.05f;
+    private Vector3 _newPosition;
+    private Quaternion _newRotation;
+
+    private void Awake()
+    {
+        _newPosition = transform.position;
+        _newRotation = transform.rotation;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _newPosition, Time.deltaTime * lerpSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _newRotation, Time.deltaTime * lerpSpeed);
+
+        if (Vector3.Distance(transform.position, _newPosition) < 1f)
+            GetNewPosition();
+    }
+
+    void GetNewPosition()
+    {
+        var xPos = Random.Range(min.x, max.x);
+        var zPos = Random.Range(min.y, max.y);
+        _newRotation = Quaternion.Euler(0, Random.Range(yRotationRange.x, yRotationRange.y), 0);
+        _newPosition = new Vector3(xPos, 0, zPos);
+    }
+}
---
+++ Software/Produkt/CarrierTracking/Assets/TabGroup.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public Color tabIdle;
+    public Color tabHover;
+    public Color tabActive;
+    public TabButton selectedTab;
+    public List<GameObject> objectsToSwap;
+
+    public void Subscribe(TabButton button)
+    {
+        if (tabButtons == null)
+        {
+            tabButtons = new List<TabButton>();
+        }
+
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button)
+    {
+        ResetTabs();
+        if (selectedTab == null || button != selectedTab)
+        {
+            button.background = tabHover;
+        }
+    }
+
+    public void OnTabExit(TabButton button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+        if (selectedTab != null)
+            selectedTab.Deselect();
+
+        selectedTab = button;
+        selectedTab.Select();
+
+        ResetTabs();
+        button.background = tabActive;
+        int index = button.transform.GetSiblingIndex();
+        for (int i=0; i<objectsToSwap.Count; i++)
+        {
+            if (i == index)
+                objectsToSwap[i].SetActive(true);
+            else
+                objectsToSwap[i].SetActive(false);
+            
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach(TabButton button in tabButtons)
+        {
+            if(selectedTab !=null && button == selectedTab) { continue; }
+            button.background = tabIdle;
+        }
+    }
+}
