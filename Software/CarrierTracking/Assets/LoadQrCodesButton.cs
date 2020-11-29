21a01c832f76605d0e31ebc15305005470c88a4a
Sakrotae <fabian.boesel@web.de>
2020-11-29 17:23:33 +0100

    QR Code scene hinzugefügt

---
+++ Software/CarrierTracking/Assets/LoadQrCodesButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadQrCodesButton : MonoBehaviour
+{
+    int i = 1;
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(onClick);
+    }
+
+    public void onClick()
+    {
+        var defaultPlane = GameObject.Find("Default Background Plane");
+        
+        
+        var qrCodes = QrCodeRecognition.getCodesFromPic("Assets/Images/0"+ i +"_2DPlan.png");
+        if (i < 4)
+            i++;
+
+        foreach(var qrCode in qrCodes)
+        {
+
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(qrCode.X, qrCode.Y);
+           
+            
+        }
+
+
+    }
+
+}
