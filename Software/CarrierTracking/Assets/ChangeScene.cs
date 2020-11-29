a6236acd75f4e7a97d8b343a6ac498406d067b5d
ViDo00 <72261969+ViDo00@users.noreply.github.com>
2020-11-29 16:16:12 +0100

    ChanceScene
    ChangeScene on Station1 click - exchanged image to button

---
+++ Software/CarrierTracking/Assets/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void changeScene(string Scenename)
+    {
+        Application.LoadLevel(Scenename);
+    }
+}
