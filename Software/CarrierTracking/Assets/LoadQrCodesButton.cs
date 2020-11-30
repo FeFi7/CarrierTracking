f9fd714e1f3fc2238f3c4552e24a1c615255525a
Niko <51508087+nurkert@users.noreply.github.com>
2020-11-30 15:36:05 +0100

    postion carrier

--- Software/CarrierTracking/Assets/LoadQrCodesButton.cs
+++ Software/CarrierTracking/Assets/LoadQrCodesButton.cs
@@ -8,34 +8,36 @@ public class LoadQrCodesButton : MonoBehaviour
     int i = 1;
     public Button yourButton;
 
+
+
     void Start()
     {
-        Button btn = yourButton.GetComponent<Button>();
-        btn.onClick.AddListener(onClick);
+        //Button btn = yourButton.GetComponent<Button>();
+        //btn.onClick.AddListener(onClick);
     }
 
     public void onClick()
     {
-        var defaultPlane = GameObject.Find("Default Background Plane");
+        //var defaultPlane = GameObject.Find("Default Background Plane");
         
         
-        var qrCodes = QrCodeRecognition.getCodesFromPic("Assets/Images/0"+ i +"_2DPlan.png");
-        if (i < 4)
-            i++;
+        //var qrCodes = QrCodeRecognition.getCodesFromPic("Assets/Images/0"+ i +"_2DPlan.png");
+        //if (i < 4)
+        //    i++;
 
-        foreach(var qrCode in qrCodes)
-        {
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            GameObject plane = GameObject.Find("Default Background Plane");
+        //foreach(var qrCode in qrCodes)
+        //{
+        //    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //    GameObject plane = GameObject.Find("Default Background Plane");
 
-            cube.transform.SetParent(plane.transform, true);
+        //    cube.transform.SetParent(plane.transform, true);
 
 
-
-            cube.transform.position = new Vector3(qrCode.X / plane.transform.localScale.x, 0, qrCode.Y / plane.transform.localScale.z);
+            
+        //    cube.transform.position = new Vector3(qrCode.X / plane.transform.localScale.x, 0, qrCode.Y / plane.transform.localScale.z);
             
        
             
-        }
+        //}
     }
 }
--- Software/CarrierTracking/Assets/Scripts/VisualStations/CarrierHandler.cs
+++ Software/CarrierTracking/Assets/Scripts/VisualStations/CarrierHandler.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CarrierHandler : MonoBehaviour
 {
     public GameObject sampleCarrier;
-    public GameObject sampleArea;
+    public GameObject AreasParent;
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +16,66 @@ public class CarrierHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        //var qrCodes = QrCodeRecognition.getCodesFromPic("Assets/Images/01_2DPlan.png");
+
+        float maxX = 831.0f;
+        float maxY = 605.0f;
+
+        float percentX = 1, percentY = 1;
+
+        //foreach (var qrCode in qrCodes)
+        //{
+        //    percentX = 100.0f / maxX * qrCode.X;
+        //    percentY = 100.0f / maxY * qrCode.Y;
+        //}
+
+        percentX = 23;
+        percentY = 24;
+
+        foreach (Transform child in AreasParent.transform)
+        {
+            GameObject area = child.gameObject;
+            PositionRelativeTo(sampleCarrier, area, percentX, percentY);
+        }
+           
     }
 
-    public static void PositionRelativeTo(GameObject carrier, GameObject area, int percentX, int percentY)
+
+    public void PositionRelativeTo(GameObject carrier, GameObject area, float percentX, float percentZ)
     {
+        float offsetX = area.transform.localScale.x / 100.0f * percentX;
+        float offsetZ = area.transform.localScale.z / 100.0f * percentZ;
 
-        
-        //Vector3 location = new Vector3(area., transform.position.y, GetMouseWorldPos().z + mOffset.z)
+        float rotation = area.transform.rotation.eulerAngles.y;
 
-        //carrier.transform.position = ;
+        offsetX -= (area.transform.localScale.x / 2);
+        offsetZ -= (area.transform.localScale.z / 2);
+
+        if(rotation == 180)
+        {
+            offsetX = -offsetX;
+            offsetZ = -offsetZ;
+        } else if(rotation > 0)
+        {
+            if(rotation == 90)
+            {
+                float chache = offsetX;
+                offsetX = offsetZ;
+                offsetZ = -chache;
+            }
+            else if (rotation == 270)
+            {
+                float chache = offsetX;
+                offsetX = -offsetZ;
+                offsetZ = chache;
+            }
+        }
+
+        carrier.transform.position = area.transform.position;
+        carrier.transform.position = new Vector3(carrier.transform.position.x + offsetX,
+            area.transform.position.y + (carrier.transform.localScale.y / 2), carrier.transform.position.z - offsetZ);
+
     }
+
 }
