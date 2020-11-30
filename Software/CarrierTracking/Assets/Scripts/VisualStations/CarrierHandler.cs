476aeb84f6b6b63828e48d6234d55b56201fc929
Niko <51508087+nurkert@users.noreply.github.com>
2020-11-30 13:45:04 +0100

    CarrierHandler alpha

---
+++ Software/CarrierTracking/Assets/Scripts/VisualStations/CarrierHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrierHandler : MonoBehaviour
+{
+    public GameObject sampleCarrier;
+    public GameObject sampleArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PositionRelativeTo(GameObject carrier, GameObject area, int percentX, int percentY)
+    {
+
+        
+        //Vector3 location = new Vector3(area., transform.position.y, GetMouseWorldPos().z + mOffset.z)
+
+        //carrier.transform.position = ;
+    }
+}
