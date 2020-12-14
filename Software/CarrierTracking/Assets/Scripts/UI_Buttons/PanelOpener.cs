83eee198963ecdffabf1a8855e93c66541baf83e
Ment <netwig@mail.hs-ulm.de>
2020-12-14 12:20:51 +0100

    Einfache Button Funktionalität hinzugefügt
    Button Add Station Funktionalität implementiert
    Button Add Ladungsträger Funktionalität implementiert
    Button Options Funktionalität implementiert
    Button Quit Funktionalität implementiert
    Button Decline Funktionalität implementiert

---
+++ Software/CarrierTracking/Assets/Scripts/UI_Buttons/PanelOpener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+
+    public void ClosePanel()
+    {
+        Panel.SetActive(false);
+    }
+}
