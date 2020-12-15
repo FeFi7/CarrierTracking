fd1d68893ec25a217175260930bc1eb79b375997
Ment <netwig@mail.hs-ulm.de>
2020-12-15 10:30:19 +0100

    Quit Funktion hinzugefügt
    Button Quit -> Accept Funktionalität hinzugefügt

---
+++ Software/CarrierTracking/Assets/Scripts/UI_Buttons/QuitCarriertracking.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitCarriertracking : MonoBehaviour
+{
+    public void doquitProgramm()
+    {
+        Debug.Log("Carriertacking has been closed.");
+        Application.Quit();
+    }
+}
