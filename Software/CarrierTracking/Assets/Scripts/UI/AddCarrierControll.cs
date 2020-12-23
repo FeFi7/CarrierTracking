c95276e927b822fef69ee1bf01af706653100305
Ment <netwig@mail.hs-ulm.de>
2020-12-23 13:34:44 +0100

    Add Carrier Panel hinzugefügt
    Add Carrier Panel hinzugefügt + Funktionalitäten

---
+++ Software/CarrierTracking/Assets/Scripts/UI/AddCarrierControll.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.IO;
+using UnityEngine.UI;
+using UnityEditor;
+using System.IO;
+
+public class AddCarrierControll : MonoBehaviour
+{
+    string path = "";
+    public GameObject Panel; 
+    public InputField CarrierName;
+    public InputField CarrierID;
+    public Dropdown DropStation;
+    public Text Model;
+    public InputField Info;
+
+    public StatusController statusfield;
+
+    public CarrierController carrierController;
+
+    //Decline Button schließt Panel und löscht Eingaben
+    public void DeclineButton()
+    {
+        ClearFields();
+        ClosePanel();
+    }
+
+    //Ruft CarrierController auf und erstellt neuen Button
+    public void AddCarrierButton(string name)
+    {
+        carrierController.AddNewButton(name);
+    }
+
+    //Addet neuen Button und speichert Eingaben in JSON File
+    public void AcceptButton()
+    {
+        try
+        {
+            AddCarrierButton(CarrierName.text);
+
+            //Funktionsaufruf Flo mit benötigten Parametern
+        }
+        catch
+        {
+            Debug.Log("Ein Fehler ist aufgetreten");
+        }
+
+        ClearFields();
+        statusfield.ChangeStatus("New Carrier added");
+        ClosePanel();
+    }
+
+    //Schließt Panel mit des Decline/Cancel Buttons
+    public void ClosePanel()
+    {
+        Panel.SetActive(false);
+    }
+
+    //Öffnet Windows Explorer um Model File auswählen zu können
+    public void OpenExplorer()
+    {
+        path = EditorUtility.OpenFilePanel("Load png Textures", "", "");
+        Model.text = path;
+    }
+
+    //Löscht Eingaben aus Panel
+    public void ClearFields()
+    {
+        CarrierName.text = "";
+        CarrierID.text = "";
+        Model.text = " ";
+        Info.text = "";
+    }
+}
