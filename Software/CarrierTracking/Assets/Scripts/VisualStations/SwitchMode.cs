38349481d42816164b0583a2071330182b44bad8
ViDo00 <72261969+ViDo00@users.noreply.github.com>
2021-01-13 20:24:46 +0100

    UI update + fixes
    + button for mode switching (edit/view)
    + selected station colored red in list
    + dropdown text bigger in options (cycle time)

---
+++ Software/CarrierTracking/Assets/Scripts/VisualStations/SwitchMode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchMode : MonoBehaviour
+{
+    public Camera editCam;
+    public Camera mainCam;
+    public Button SwitchCameraButton = null;
+
+    void Start()
+    {
+        SwitchCameraButton = gameObject.GetComponent<Button>();
+        SwitchCameraButton.onClick.AddListener(switchCamera);
+    }
+
+    //Switch camnera on button-click and change its text
+    void switchCamera()
+    {
+        if (editCam.enabled)
+        {
+            editCam.enabled = false;
+            mainCam.enabled = true;
+
+            mainCam.transform.rotation = Quaternion.Euler(90, 0, 0);
+
+            GameObject.Find("ButtonModusWechseln").GetComponentInChildren<Text>().text = "zu Bearbeitungsmodus wechseln";
+        }
+        else
+        {
+            editCam.enabled = true;
+            mainCam.enabled = false;
+            GameObject.Find("ButtonModusWechseln").GetComponentInChildren<Text>().text = "zurück zur Übersicht";
+        }
+    }
+}
